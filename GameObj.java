b2d3e0a40d72c16d26b652d9a957d2a0167b3288
Jeffrey Chen <jeffche@seas.upenn.edu>
2017-12-21 14:00:28 -0500

    Update GameObj.java

--- GameObj.java
+++ GameObj.java
@@ -1,26 +1,7 @@
-/**
- * CIS 120 Game HW
- * (c) University of Pennsylvania
- * @version 2.1, Apr 2017
- */
-
 import java.awt.Graphics;
 
-/** 
- * An object in the game. 
- *
- * Game objects exist in the game court. They have a position, velocity, size and bounds. Their
- * velocity controls how they move; their position should always be within their bounds.
- */
 public abstract class GameObj {
-    /*
-     * Current position of the object (in terms of graphics coordinates)
-     *  
-     * Coordinates are given by the upper-left hand corner of the object. This position should
-     * always be within bounds.
-     *  0 <= px <= maxX 
-     *  0 <= py <= maxY 
-     */
+
     private int px; 
     private int py;
 
@@ -32,16 +13,9 @@ public abstract class GameObj {
     private int vx;
     private int vy;
 
-    /* 
-     * Upper bounds of the area in which the object can be positioned. Maximum permissible x, y
-     * positions for the upper-left hand corner of the object.
-     */
     private int maxX;
     private int maxY;
 
-    /**
-     * Constructor
-     */
     public GameObj(int vx, int vy, int px, int py, int width, int height, int courtWidth,
         int courtHeight) {
         this.vx = vx;
@@ -103,19 +77,11 @@ public abstract class GameObj {
 
     /*** UPDATES AND OTHER METHODS ****************************************************************/
 
-    /**
-     * Prevents the object from going outside of the bounds of the area designated for the object.
-     * (i.e. Object cannot go outside of the active area the user defines for it).
-     */ 
     private void clip() {
         this.px = Math.min(Math.max(this.px, 0), this.maxX);
         this.py = Math.min(Math.max(this.py, 0), this.maxY);
     }
 
-    /**
-     * Moves the object by its velocity.  Ensures that the object does not go outside its bounds by
-     * clipping.
-     */
     public void move() {
         this.px += this.vx;
         this.py += this.vy;
@@ -123,15 +89,6 @@ public abstract class GameObj {
         clip();
     }
 
-    /**
-     * Determine whether this game object is currently intersecting another object.
-     * 
-     * Intersection is determined by comparing bounding boxes. If the bounding boxes overlap, then
-     * an intersection is considered to occur.
-     * 
-     * @param that The other object
-     * @return Whether this object intersects the other object.
-     */
     public boolean intersects(GameObj that) {
         return (this.px + this.width >= that.px
             && this.py + this.height >= that.py
@@ -139,17 +96,6 @@ public abstract class GameObj {
             && that.py + that.height >= this.py);
     }
 
-
-    /**
-     * Determine whether this game object will intersect another in the next time step, assuming
-     * that both objects continue with their current velocity.
-     * 
-     * Intersection is determined by comparing bounding boxes. If the  bounding boxes (for the next
-     * time step) overlap, then an intersection is considered to occur.
-     * 
-     * @param that The other object
-     * @return Whether an intersection will occur.
-     */
     public boolean willIntersect(GameObj that) {
         int thisNextX = this.px + this.vx;
         int thisNextY = this.py + this.vy;
@@ -162,13 +108,6 @@ public abstract class GameObj {
             && thatNextY + that.height >= thisNextY);
     }
 
-
-    /**
-     * Update the velocity of the object in response to hitting an obstacle in the given direction.
-     * If the direction is null, this method has no effect on the object.
-     *
-     * @param d The direction in which this object hit an obstacle
-     */
     public void bounce(Direction d) {
         if (d == null) return;
         
@@ -189,13 +128,7 @@ public abstract class GameObj {
         		break;
         }
     }
-
-    /**
-     * Determine whether the game object will hit a wall in the next time step. If so, return the
-     * direction of the wall in relation to this game object.
-     *  
-     * @return Direction of impending wall, null if all clear.
-     */
+    
     public Direction hitWall() {
         if (this.px + this.vx < 0) {
             return Direction.LEFT;
@@ -212,13 +145,6 @@ public abstract class GameObj {
         }
     }
 
-    /**
-     * Determine whether the game object will hit another object in the next time step. If so,
-     * return the direction of the other object in relation to this game object.
-     * 
-     * @param that The other object
-     * @return Direction of impending object, null if all clear.
-     */
     public Direction hitObj(GameObj that) {
         if (this.willIntersect(that)) {
             double dx = that.px + that.width / 2 - (this.px + this.width / 2);
@@ -244,14 +170,5 @@ public abstract class GameObj {
         }
     }
 
-    /**
-     * Default draw method that provides how the object should be drawn in the GUI. This method does
-     * not draw anything. Subclass should override this method based on how their object should
-     * appear.
-     * 
-     * @param g The <code>Graphics</code> context used for drawing the object. Remember graphics
-     * contexts that we used in OCaml, it gives the context in which the object should be drawn (a
-     * canvas, a frame, etc.)
-     */
     public abstract void draw(Graphics g);
 }
